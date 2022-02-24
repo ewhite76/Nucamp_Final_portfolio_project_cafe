8df79b97445f365a0703fd037b740617f3e9092f
ewhite76 <71043686+ewhite76@users.noreply.github.com>
2022-02-24 12:19:55 -0600

    first push of project to GH

---
+++ pp_hello_django_docker_compose/data/misc/django_init.sql
@@ -0,0 +1,13 @@
+DROP DATABASE IF EXISTS pp_cafe_priorities_db;
+CREATE DATABASE pp_cafe_priorities_db;
+
+\c pp_cafe_priorities_db
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+SET default_tablespace = '';
+SET default_with_oids = false;
